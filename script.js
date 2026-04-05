8cc268ecbe5daaeb978119af6a61721787ae0394
6duriech9 <vabond.fx@gmail.com>
2026-04-06 00:09:46 +0300

    update UI + dark mode

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// ==========================================
+// ТЕМНАЯ ТЕМА
+// ==========================================
+const themeBtn = document.getElementById('theme-toggle');
+// Проверяем, есть ли сохраненная тема в localStorage
+const currentTheme = localStorage.getItem('theme');
+
+// Если тема была сохранена, применяем её
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+}
+
+themeBtn.addEventListener('click', () => {
+    // Получаем текущую тему
+    let theme = document.documentElement.getAttribute('data-theme');
+    
+    // Переключаем
+    if (theme === 'dark') {
+        document.documentElement.removeAttribute('data-theme');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+});
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const fadeElements = document.querySelectorAll('.fade-up');
     
@@ -27,10 +55,31 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+// ==========================================
+// КНОПКА "НАВЕРХ" И ЧИСТЫЙ URL ПРИ КЛИКАХ
+// ==========================================
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+// Показываем/прячем кнопку при скролле
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 500) { // Показываем после 500px скролла
+        scrollTopBtn.classList.add('is-visible');
+    } else {
+        scrollTopBtn.classList.remove('is-visible');
+    }
+});
+
+// Плавный скролл наверх
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault(); // Останавливаем стандартный переход с добавлением # в URL
-
         const targetId = this.getAttribute('href');
         
         // Если это просто ссылка наверх (логотип)
@@ -56,6 +105,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
                 top: offsetPosition,
                 behavior: 'smooth'
             });
+
+            history.pushState(null, null, window.location.pathname); 
         }
     });
 });
+
+
